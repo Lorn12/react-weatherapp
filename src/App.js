c44ca3cb0307094ee7d9e675a68092e75d578cc3
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-05 12:42:33 -0600

    Added Github repo link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+
+      <footer>
+        This project was coded my{" "}
+        <a
+          href="https://www.shecodes.io/graduates/76312-lauren-wooten"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Lauren Wooten{" "}
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/Lorn12/react-weatherapp"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
