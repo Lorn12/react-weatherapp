6cc5bee594a1577af007f85ee75d12996595deb3
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-07 17:34:06 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <div className="container">
+        <h1>Weather app</h1>
 
-      <footer>
-        This project was coded my{" "}
-        <a
-          href="https://www.shecodes.io/graduates/76312-lauren-wooten"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Lauren Wooten{" "}
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/Lorn12/react-weatherapp"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Github
-        </a>
-      </footer>
+        <footer>
+          This project was coded my{" "}
+          <a
+            href="https://www.shecodes.io/graduates/76312-lauren-wooten"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Lauren Wooten{" "}
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Lorn12/react-weatherapp"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
