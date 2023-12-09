02eea895f4a08a9ba5e27422b2f5a99b5000076a
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-09 10:19:40 -0600

    Implemented weather icons from API

--- src/Weather.js
+++ src/Weather.js
@@ -14,8 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl:
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png",
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
