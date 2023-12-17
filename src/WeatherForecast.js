2a5d58339488de17622533f27eb2d833e9c7feee
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-16 18:46:54 -0600

    Implemented 5 day weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,10 +17,16 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            {/* //COMPONENT INTEGRATIONS ________________________ */}
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  {/* //COMPONENT INTEGRATIONS ________________________ */}
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
