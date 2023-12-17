582c6d9374df95505e5b4a98308406eb3bcd8ecc
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-16 19:11:59 -0600

    Implemented useEffect for coordinates refresh

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import Axios from "axios";
 
-//WEATHER FORECAST DATA COMPONENT ____________________________________________________
+//WEATHER FORECAST COORDINATES DATA COMPONENT ________________________________________
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
